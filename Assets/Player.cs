38f1eda9919dfc8126b7a85d51fcd9445d4dad1c
alejandrocy99 <moflycy@gmail.com>
2024-11-13 20:20:53 +0100

    agente dirigiendose a donde marco con el raton

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
-        
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit = new RaycastHit();
+            if(Physics.Raycast(ray,out hit,1000))
+            {
+                GetComponent<NavMeshAgent>().SetDestination(hit.point);
+            }
+        }
     }
 }
