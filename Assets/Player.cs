4ff38fbb406732f77967639b90874bd588d34a8a
alejandrocy99 <moflycy@gmail.com>
2024-11-13 21:19:51 +0100

    ruta aleatoria

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,23 +5,35 @@ using UnityEngine.AI;
 
 public class Player : MonoBehaviour
 {
-    
+    Vector3 destination;
+    public Transform routeFather;
+    int indexChildren;
     void Start()
     {
-        
+        destination = routeFather.GetChild(indexChildren).position;
+        GetComponent<NavMeshAgent>().SetDestination(destination);
     }
 
-   
-    void Update()
+
+    private void Update()
     {
-        if(Input.GetButtonDown("Fire1"))
+
+        //if(Input.GetButtonDown("Fire1"))
+        //{
+        //    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //   RaycastHit hit = new RaycastHit();
+        //    {
+        //       GetComponent<NavMeshAgent>().SetDestination(hit.point);
+        //  }
+        //}
+
+
+
+        if (Vector3.Distance(transform.position, destination) < 2.5f)
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit = new RaycastHit();
-            if(Physics.Raycast(ray,out hit,1000))
-            {
-                GetComponent<NavMeshAgent>().SetDestination(hit.point);
-            }
+            indexChildren = Random.Range(0,routeFather.childCount);
+            destination = routeFather.GetChild(indexChildren).position;
+            GetComponent<NavMeshAgent>().SetDestination(destination);
         }
     }
 }
